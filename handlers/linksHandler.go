dfe3b233a9e4d71b4f75abd656ba5ee858197526
seroking <baridmohamedamine004@gmail.com>
2025-09-07 03:18:17 +0100

    adding functions to link handler + some fixes

--- handlers/linksHandler.go
+++ handlers/linksHandler.go
@@ -33,10 +33,15 @@ func CreateLink(c *gin.Context, db gorm.DB) {
 		OriginalUrl: Input.OriginalUrl,
 		UserID:      userIDVal,
 	}
-	link.ShortCode = helpers.GenerateCode(strconv.Itoa(int(link.ID)))
-
 	if err := db.Create(&link).Error; err != nil {
 		c.JSON(400, gin.H{"error": "failed to created Link"})
+		return
+	}
+
+	link.ShortCode = helpers.GenerateCode(strconv.Itoa(int(link.ID)))
+
+	if err := db.Save(&link).Error; err != nil {
+		c.JSON(500, gin.H{"error": "failed to save database"})
 		return
 	}
 
@@ -45,9 +50,26 @@ func CreateLink(c *gin.Context, db gorm.DB) {
 func DeleteLink(c *gin.Context, db *gorm.DB) {
 	var link models.Link
 	id := c.Param("id")
+	userID, exist := c.Get("user_id")
+	if !exist {
+		c.JSON(404, gin.H{"error": "User not found"})
+		return
+	}
 
-	if err := db.First(&link, id).Error; err != nil {
-		c.JSON(404, gin.H{"error": "Link not found"})
+	// if err := db.First(&link, id).Error; err != nil {
+	// 	c.JSON(404, gin.H{"error": "Link not found"})
+	// 	return
+	// }
+
+	if err := db.Preload("User").First(&link, id); err != nil {
+		c.JSON(404, gin.H{"error": err})
+		return
+	}
+	userIDFloat := userID.(float64)
+	userIDVal := uint(userIDFloat)
+
+	if link.User.Role != "admin" && link.UserID != userIDVal {
+		c.JSON(401, gin.H{"error": "Forbidden"})
 		return
 	}
 
@@ -57,5 +79,19 @@ func DeleteLink(c *gin.Context, db *gorm.DB) {
 }
 
 func ListUserLinks(c *gin.Context, db *gorm.DB) {
+	var links []models.Link
 
+	userID, exist := c.Get("user_id")
+	if !exist {
+		c.JSON(401, gin.H{"error": "Unothaurized"})
+		return
+	}
+
+	userIDFloat := userID.(float64)
+
+	userIDVal := uint(userIDFloat)
+
+	db.Where("user_id = ?", userIDVal).Find(&links)
+
+	c.JSON(200, gin.H{"message": "links retrieved successfullt", "data": links})
 }
